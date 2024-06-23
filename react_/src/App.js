792bb9d7319573c3b06741b585f3bb086a0c52d6
hansanhha <kwixs35@gmail.com>
2024-06-23 17:38:07 +0900

    [react] product table 추가

--- react_/src/App.js
+++ react_/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Game from "./tic-tac-toe/Game";
 import "./App.css";
+import ProductTable from "./product-table/ProductTable";
+import {findAllByDisplayValue} from "@testing-library/react";
 
 function App() {
   return (
     <div className="App">
-        <a href={"/tic-tac-toe"}>game</a>
+        <div><a href={"/tic-tac-toe"}>game</a></div>
+        <div><a href={"/product"}>product</a></div>
         <Router>
-          <Routes>
-            <Route path={"/tic-tac-toe"} element={<Game />} />
+            <Routes>
+                <Route path={"/tic-tac-toe"} element={<Game />} />
+                <Route path={"/product"} element={<ProductTable />} />
           </Routes>
         </Router>
     </div>
--- react_/src/index.js
+++ react_/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./tic-tac-toe/game-style.css";
+import "./product-table/product-table.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
